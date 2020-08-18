6bf3d9d5de21944a9094023c428d7a9dd32c13d7
Agil89 <agil.makhmudov@mail.ru>
2020-08-18 16:14:57 +0400

    contact page ready

--- Main/urls.py
+++ Main/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from Main.views import MainClassView,SubscriberCreateView
+from Main.views import MainClassView,SubscriberCreateView,ContactSubjectView,ContactPageView
+
+app_name = 'main'
 
 urlpatterns = [
     path('',MainClassView.as_view(),name='home'),
-    path('subscribe/',SubscriberCreateView.as_view(),name='subscribe')
+    path('subscribe/',SubscriberCreateView.as_view(),name='subscribe'),
+    path('contact/',ContactPageView.as_view(),name='contact'),
+    path('contact-form/',ContactSubjectView.as_view(),name='contact-from')
 ]
--- Main/views.py
+++ Main/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.urls import reverse_lazy
-from Main.forms import SubscriberForm
-from django.views.generic import ListView,CreateView
-from Main.models import City
+from Main.forms import SubscriberForm,ContactForm
+from django.views.generic import ListView,CreateView,TemplateView
+from Main.models import City,ContactInfo
 # Create your views here.
 
 class MainClassView(ListView):
@@ -15,7 +15,7 @@ class SubscriberCreateView(CreateView):
     form_class = SubscriberForm
     template_name = None
     http_method_names = ('post',)
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('main:home')
 
     def get_success_url(self):
         redirect_url = self.request.GET.get('redirect_url',self.success_url)
@@ -29,3 +29,23 @@ class SubscriberCreateView(CreateView):
         messages.success(self.request,form.errors)
         redirect_url = self.request.GET.get('redirect_url',self.success_url)
         return redirect(redirect_url)
+
+
+class ContactSubjectView(CreateView):
+    form_class = ContactForm
+    template_name = 'contact.html'
+    # http_method_names = ('post',)
+    success_url = reverse_lazy('main:home')
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        contactInfo = ContactInfo.objects.get()
+        context['contactInfo'] = contactInfo
+        context['form'] = ContactForm()
+        return context
+
+
+
