5c82d3020e585efbce4c086e71e843c26c4d19a2
amilalizada <elizade_amil@inbox.ru>
2020-08-24 16:42:23 +0400

    tur singlle hazir

--- Tours/urls.py
+++ Tours/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from Tours.views import ToursPage
+from Tours.views import ToursPage,ToursSinglePage
 
 urlpatterns = [
     path('tours-page/',ToursPage.as_view(),name='tourspage'),  
+    path('tours-single-page/',ToursSinglePage.as_view(),name='tours_single_page'),  
+
 ]
