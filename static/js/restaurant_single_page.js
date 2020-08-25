0ed140a976e199b4dd4367a955c7d3d8ab80024b
Ssamirr <sardarlisamir@gmail.com>
2020-08-25 13:10:58 +0400

    checkbox

--- static/js/restaurant_single_page.js
+++ static/js/restaurant_single_page.js
@@ -1,28 +1,31 @@
-document.addEventListener('click', function (e) {
-    if (e.target.closest('.position-exit-cursor'))
-    {
-        document.querySelector('.table-restaurant-open-time').style.display = 'none'; 
-    }
-    else if (e.target.closest('.jquery-effect')) 
-    {
-        if(document.querySelector('.table-restaurant-open-time').style.display == 'none')
+    document.addEventListener('click', function (e) {
+        console.log('2defe')
+        e.stopPropagation();
+        // e.preventDefault();
+        if (e.target.closest('.position-exit-cursor'))
         {
-            console.log('aa')
-            document.querySelector('.table-restaurant-open-time').style.display = 'block';
+            document.querySelector('.table-restaurant-open-time').style.display = 'none'; 
         }
-        else if(document.querySelector('.table-restaurant-open-time').style.display == 'block')
+        else if (e.target.closest('.jquery-effect')) 
         {
-            console.log('bbb')
+            console.log('jquery')
+            if(document.querySelector('.table-restaurant-open-time').style.display == 'none')
+            {
+                console.log('aa')
+                document.querySelector('.table-restaurant-open-time').style.display = 'block';
+            }
+            else{
+                console.log('bbb')
+                document.querySelector('.table-restaurant-open-time').style.display = 'none';
+            }
+            
+        }
+        else if (e.target.closest('.table-restaurant-open-time')) 
+        {
+            console.log('Nothing to happened')
+        }
+        else 
+        {
             document.querySelector('.table-restaurant-open-time').style.display = 'none';
         }
-        
-    }
-    else if (e.target.closest('.table-restaurant-open-time')) 
-    {
-        console.log('Nothing to happened')
-    }
-    else 
-    {
-        document.querySelector('.table-restaurant-open-time').style.display = 'none';
-    }
-})
+    })
